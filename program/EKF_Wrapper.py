d523c2defd71921fc22292a645da5bad046abf7e
mrdinghr <mr_dinghr@163.com>
2022-05-10 22:09:46 +0200

    5.10 EKF

--- program/EKF_Wrapper.py
+++ program/EKF_Wrapper.py
@@ -1,24 +1,30 @@
-from filterpy.kalman import ExtendedKalmanFilter
-from filterpy.common import Q_discrete_white_noise
-'''
-class EKF_Wrapper:
-    def __init__(self,P ,):
-        self.P=P
+import numpy.linalg as lg
 
-    def predict_state(self,state,u,system,table):
-        hascollision, state, jacobian=table.applycollision(state)
-        if hascollision:
-            system.update_jacobian(state, u)
-            state=system.f(state, u)
-        self.P= system.F*self.P*system.F.T
-'''
-def EKF_Wrapper(state, table, system, observation, measure):
-    dt= system.dt
-    rk=ExtendedKalmanFilter(dim_x=6, dim_z=3, dim_u=1)
-    rk.x=state
-    rk.F=system.F
-    #rk.R=
-    #rk.Q
-    #rk.P
-    rk.update(measure, observation.H, observation.observation)
+class air_hockey_EKF:
+    def __init__(self, state, u, system, table, observation, Q, R, P):
+        self.state = state
+        self.system = system
+        self.table = table
+        self.observation = observation
+        self.Q = Q
+        self.R = R
+        self.P = P
+        self.u = u
+        self.predict_state=None
+
+    def predict(self, state, u):
+        self.P = self.system.F @ self.P @ self.system.F.T + self.Q
+        self.predict_state = self.system.f(self.state, self.u)
+
+    def update(self,z):
+        #measurement residual
+        y=z-self.observation(self.state)
+        S=self.P+self.R #innovation covariance  because H is identify matrix so ignored
+        K=self.P@lg.inv(S)
+        self.state=self.predict_state+K@y
+        self.P=self.F@self.P@self.F.T+self.Q
+
+
+
+
 
--- program/test.py
+++ program/test.py
@@ -4,16 +4,16 @@ import air_hockey_baseline
 from air_hockey_plot import trajectory_plot as trajectory
 
 table = air_hockey_baseline.AirHockeyTable(length=2, width=1, goalWidth=0.2, puckRadius=0.05, restitution=0.7,
-                                           rimFriction=0.1, dt=0.01)
-system = air_hockey_baseline.SystemModel(tableDamping=0.1, tableFriction=0.01, tableLength=2, tableWidth=1,
+                                           rimFriction=0.5, dt=0.01)
+system = air_hockey_baseline.SystemModel(tableDamping=0., tableFriction=0.4, tableLength=2, tableWidth=1,
                                          goalWidth=0.2, puckRadius=0.05, malletRadius=0.1, tableRes=0.7, malletRes=0.7,
-                                         rimFriction=0.1, dt=0.01)
+                                         rimFriction=0.5, dt=0.01)
 
 jacobian = np.eye(6)
 
 u = 0.01
-trajectory(table=table, system=system, u=u, x=0.5, y=0, dx=2, dy=2, theta=2, d_theta=1, x_var=0.05, y_var=0.05,
-           dx_var=0, dy_var=0, theta_var=0, d_theta_var=0, state_num=200,
-           puck_num=10, touchline=True, touch_line_x=2, touch_line_y=-0.1)
-plt.title("x y as gaussian variable")
+trajectory(table=table, system=system, u=u, x=0.5, y=0, dx=10, dy=1, theta=2, d_theta=0., x_var=0.0, y_var=0.0,
+           dx_var=0., dy_var=0., theta_var=0, d_theta_var=5, state_num=1000,
+           puck_num=1, touchline=False, touch_line_x=2, touch_line_y=2)
+plt.title("angular velocity as gaussian variable")
 plt.show()
